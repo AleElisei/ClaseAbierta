fe71ae4e9f2cca2da899890d5b9653d2b3e0a508
Ale <alejandroelisei@gmail.com>
2026-06-28 18:00:35 -0300

    ataque enemigos, barra de vida

--- SkeletonSurvivors/Assets/Scripts/Enemy.cs
+++ SkeletonSurvivors/Assets/Scripts/Enemy.cs
@@ -7,17 +7,21 @@ public class Enemy : MonoBehaviour
     [SerializeField] Transform player;
 
     [SerializeField] float speed;
-    [SerializeField] float radius;
+    [SerializeField] float detectionRadius;
+    [SerializeField] float attackRadius;
+    [SerializeField] LayerMask playerLayer;
 
     [SerializeField] int life = 1;
 
     private Animator animator;
+    private float actualSpeed;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         target = GameObject.FindWithTag("Home").transform;
         player = GameObject.FindWithTag("Player").transform;
+        actualSpeed = speed;
     }
 
     void Update()
@@ -26,12 +30,17 @@ public class Enemy : MonoBehaviour
 
         Vector3 dir = player.position - transform.position;
 
-        if (dir.magnitude > radius)
+        if(dir.magnitude < attackRadius)
+        {
+            animator.SetTrigger("Attack");
+            actualSpeed = 0;
+        }
+        else if (dir.magnitude > detectionRadius)
         {
             dir = target.position - transform.position;
         }
         dir.y = 0;
-        transform.position += dir.normalized * speed * Time.deltaTime;
+        transform.position += dir.normalized * actualSpeed * Time.deltaTime;
         transform.forward = dir;
 
     }
@@ -49,6 +58,26 @@ public class Enemy : MonoBehaviour
         animator.SetTrigger("Hit");
     }
 
+    public void Attack()
+    {
+        if(Physics.Raycast(transform.position + Vector3.up * 1.5f, transform.forward, out RaycastHit hit, (attackRadius + 0.5f), playerLayer)) {
+            Player p = hit.collider.GetComponent<Player>();
+            if (p != null)
+            {
+                p.GetDamage(1);
+            }
+        }
+
+        Debug.DrawLine(transform.position + Vector3.up * 1.5f,
+                       transform.position + Vector3.up * 1.5f + transform.forward * (attackRadius + 0.5f),
+                       Color.red, 1);
+    }
+
+    public void StopAttack()
+    {
+        actualSpeed = speed;
+    }
+
     public void Die()
     {
         speed = 0;
@@ -58,6 +87,6 @@ public class Enemy : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position,radius);
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
 }
--- SkeletonSurvivors/Assets/Scripts/Player.cs
+++ SkeletonSurvivors/Assets/Scripts/Player.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 
     [SerializeField] InputActionReference moveAction;
     [SerializeField] float speed = 1;
+    [SerializeField] int life = 5;
+    [SerializeField] int currentLife = 5;
 
     [SerializeField] Transform spawnBullet;
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] Animator animator;
+    [SerializeField] Image barLife;
 
     [SerializeField] GameObject bow;
 
@@ -34,6 +38,26 @@ public class Player : MonoBehaviour
 
     }
 
+    public void GetDamage(int damage)
+    {
+        currentLife -= damage;
+        barLife.fillAmount = (float)currentLife / (float)life;
+        Debug.Log(currentLife / life);
+
+        if (currentLife <= 0)
+        {
+            Die();
+            return;
+        }
+        animator.SetTrigger("Hit");
+    }
+
+    public void Die()
+    {
+        speed = 0;
+        animator.SetBool("Death", true);
+        Destroy(gameObject, 2);
+    }
     public void Shoot()
     {
         Instantiate(bulletPrefab, spawnBullet.position, spawnBullet.rotation);
